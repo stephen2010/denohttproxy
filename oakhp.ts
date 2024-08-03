81adcf98ca6cd45e8ab05b99ac4944c880cfc1fb
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-03 11:30:41 +0800

    Add files via upload

---
+++ oakhp.ts
@@ -0,0 +1,8 @@
+import { proxy } from "https://deno.land/x/oak_http_proxy@2.3.0/mod.ts";
+import { Application } from "https://deno.land/x/oak@v12.6.2/mod.ts";
+
+const app = new Application();
+
+app.use(proxy("https://github.com/"));
+
+await app.listen({ port: 3000 });
