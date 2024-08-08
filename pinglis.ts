2be1a77608173746a769f6c39b711735235a5627
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-08 17:17:08 +0800

    echo

--- pinglis.ts
+++ pinglis.ts
@@ -2,8 +2,8 @@ const hostport = { port: 80};
 const listener = Deno.listen(hostport);
 
 for await (const conn of listener) {
-  await conn.readable.pipeTo(Deno.stdout.writable);
-  // await conn.readable.pipeTo(conn.writable);
+//  await conn.readable.pipeTo(Deno.stdout.writable);
+  await conn.readable.pipeTo(conn.writable);
 
 //  conn.close();
 }
