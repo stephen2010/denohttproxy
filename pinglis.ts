df30d21a25dbb855a2927d64a20af04d55b02607
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-08 17:05:14 +0800

    Create pinglis.ts

---
+++ pinglis.ts
@@ -0,0 +1,9 @@
+const hostport = { port: 80};
+const listener = Deno.listen(hostport);
+
+for await (const conn of listener) {
+  await conn.readable.pipeTo(Deno.stdout.writable);
+  // await conn.readable.pipeTo(conn.writable);
+
+  conn.close();
+}
