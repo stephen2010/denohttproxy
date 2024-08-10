ee06398425557a9cf57e662743174f2d6c468014
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-10 23:53:38 +0800

    1

--- chartfn.ts
+++ chartfn.ts
@@ -5,15 +5,17 @@ export default async function chartfn() {
 	return res;
 }
 */
-import { serveFile } from 'jsr:@std/http/file-server';
+import { serveFile } from 'jsr:@std/http/file-server.ts';
 
-export default async function chartfn(req: Request) {
+export async function chartfn(req: Request) {
 	return serveFile(
 		req,
 		'./tv.html',
 	);
+}
 
-	// const tvurl = 'https://tradingview.com/chart';
-	// const res = await fetch(tvurl);
-	// return res;
+export async function chartfn(path1: string) {
+	const tvurl = 'https://static.tradingview.com/';
+	const res = await fetch(tvurl + path1);
+	return res;
 }
