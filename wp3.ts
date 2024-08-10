248df32a3a902c793670ccf0f9b8a01869ce6e46
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-10 12:41:42 +0800

    1

--- wp3.ts
+++ wp3.ts
@@ -1,32 +1,32 @@
-
 const luyou = (req: Request) => {
-	const pathname = new URL(req.url).pathname;
-	const pathsz = pathname.split('/');
-	let path1 = pathsz.shift();
-	if (pathsz.length != 0) {
-		path1 = pathsz.shift();
-	}
-	switch (path1) {
-		case 'chart': {
-			const tvurl = "https://tradingview.com/chart";
-			 return  fetch(tvurl);
-		}
-		case 'static': {
-			break;
-		}
-		// case 'socket.io': {
-		// 	return socketiowebsocket(req);
-		// }
-		// case 'message-pipe-ws': {
-		// 	return messagepipews(req);
-		// }
-		default: {
-			// console.log('luyou:', path1, pathsz);
-			return new Response('404: Not Found', {
-				status: 404,
-			});
-		}
-	}
-}
+  const pathname = new URL(req.url).pathname;
+  const pathsz = pathname.split("/");
+  let path1 = pathsz.shift();
+  if (pathsz.length != 0) {
+    path1 = pathsz.shift();
+  }
+  switch (path1) {
+    case "chart": {
+      const tvurl = "https://tradingview.com/chart";
+      const res = await fetch(tvurl);
+      return res;
+    }
+    case "static": {
+      break;
+    }
+    // case 'socket.io': {
+    // 	return socketiowebsocket(req);
+    // }
+    // case 'message-pipe-ws': {
+    // 	return messagepipews(req);
+    // }
+    default: {
+      // console.log('luyou:', path1, pathsz);
+      return new Response("404: Not Found", {
+        status: 404,
+      });
+    }
+  }
+};
 
 Deno.serve(luyou);
