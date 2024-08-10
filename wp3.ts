66ff9ac99edd3c05f10b26fd611a118d88c7e14d
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-10 22:36:48 +0800

    worker

--- wp3.ts
+++ wp3.ts
@@ -1,32 +1,35 @@
+const wsworker = new Worker(import.meta.resolve('./wsworker.ts'), {
+	type: 'module',
+});
+wsworker.onmessage = async (e) => {
+	const path1 = e.data;
+	switch (path1) {
+		case 'chart': {
+			const tvurl = 'https://tradingview.com/chart';
+			const res = await fetch(tvurl);
+			return res;
+		}
+		case 'static': {
+			break;
+		}
+		// case 'socket.io': {
+		//  return socketiowebsocket(req);
+		// }
+		// case 'message-pipe-ws': {
+		//  return messagepipews(req);
+		// }
+		default: {
+			// console.log('luyou:', path1, pathsz);
+			return new Response('404: Not Found', {
+				status: 404,
+			});
+		}
+	}
+};
+
 const luyou = async (req: Request) => {
-  const pathname = new URL(req.url).pathname;
-  const pathsz = pathname.split("/");
-  let path1 = pathsz.shift();
-  if (pathsz.length != 0) {
-    path1 = pathsz.shift();
-  }
-  switch (path1) {
-    case "chart": {
-      const tvurl = "https://tradingview.com/chart";
-      const res = await fetch(tvurl);
-      return res;
-    }
-    case "static": {
-      break;
-    }
-    // case 'socket.io': {
-    // 	return socketiowebsocket(req);
-    // }
-    // case 'message-pipe-ws': {
-    // 	return messagepipews(req);
-    // }
-    default: {
-      // console.log('luyou:', path1, pathsz);
-      return new Response("404: Not Found", {
-        status: 404,
-      });
-    }
-  }
+	const pathname = new URL(req.url).pathname;
+	wsworker.postMessage(pathname);
 };
 
 Deno.serve(luyou);
