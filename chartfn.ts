545065937e210fbbc799fe068c5d0c80bab27930
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-10 23:25:56 +0800

    indexhtml

--- chartfn.ts
+++ chartfn.ts
@@ -1,5 +1,20 @@
+/*
 export default async function chartfn() {
 	const tvurl = 'https://tradingview.com/chart';
 	const res = await fetch(tvurl);
 	return res;
+}
+*/
+const indexhtml = await Deno.readTextFile(
+	'./tv.html',
+);
+
+export default async function chartfn() {
+	return new Response(indexhtml, {
+		status: 200,
+	});
+
+	// const tvurl = 'https://tradingview.com/chart';
+	// const res = await fetch(tvurl);
+	// return res;
 }
