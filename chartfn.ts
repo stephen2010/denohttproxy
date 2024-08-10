122165087ede2ce2f89cd809cd55ff4116410fc6
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-11 01:18:12 +0800

    1

--- chartfn.ts
+++ chartfn.ts
@@ -19,3 +19,35 @@ export async function staticfn(path1: string) {
 	const res = await fetch(tvurl + path1);
 	return res;
 }
+
+const ws1out = new WebSocket('wss://data.tradingview.com');
+
+export function socketiowebsocket(req: Request) {
+	const { socket: ws1, response } = Deno.upgradeWebSocket(req);
+	ws1out.onmessage = (e) => {
+		const shuju = e.data;
+		ws1.send(shuju);
+	};
+	ws1out.onopen = () => {};
+	ws1out.onerror = (e) => {
+		ws1out.close();
+	};
+	ws1out.onclose = () => {
+	};
+
+	ws1.onopen = () => {
+	};
+	ws1.onmessage = (e) => {
+		const shuju = e.data;
+		ws1out.send(shuju);
+	};
+	ws1.onerror = (e) => {
+		ws1.close();
+	};
+	ws1.onclose = () => {
+	};
+	return response;
+}
+
+export function messagepipews(req: Request) {
+}
