e21f94e0a1c80280291ad9f3f4949b8e7015111e
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-11 00:32:35 +0800

    1

--- chartfn.ts
+++ chartfn.ts
@@ -15,7 +15,7 @@ export async function chartfn(req: Request) {
 }
 
 export async function staticfn(path1: string) {
-	const tvurl = 'https://static.tradingview.com/';
+	const tvurl = 'https://static.tradingview.com';
 	const res = await fetch(tvurl + path1);
 	return res;
 }
